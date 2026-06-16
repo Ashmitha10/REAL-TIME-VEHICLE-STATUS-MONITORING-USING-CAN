8e25b9578264952b303e26c0c089b82e75ecbf45
KORRA ASHMITHA <korraasmitha@gmail.com>
2026-06-16 15:37:59 +0530

    Update I2C.h

--- MAJOR_PROJECT/I2C/I2C.h
+++ MAJOR_PROJECT/I2C/I2C.h
@@ -1,13 +1,28 @@
-#ifndef I2C_H
-#define I2C_H
+#ifndef I2C_H // Checks whether I2C_H is already defined or not
+#define I2C_H // Defines I2C_H to avoid multiple inclusion of this header file
 
-#include "types.h"
+
+#include "types.h" // Includes user-defined data types like u8, u16, u32
+// Function declaration to initialize the I2C peripheral
 void Init_i2c(void);
+
+// Function declaration to generate I2C START condition
 void i2c_start(void);
+
+// Function declaration to generate I2C REPEATED START condition
 void i2c_restart(void);
+
+// Function declaration to generate I2C STOP condition
 void i2c_stop(void);
+
+// Function declaration to send/write one byte of data through I2C
 void i2c_write(u8 data);
+
+// Function declaration to read a byte and send NACK (No Acknowledge)
 u8 i2c_nack(void);
+
+// Function declaration to read a byte and send ACK (Master Acknowledge)
 u8 i2c_mack(void);
 
+// Ends the header guard condition
 #endif
